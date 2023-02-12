d0b80efcf2252e696b0774688fb5a3960399738d
Uğur kellecioğlu <ugur.kellecioglu@outlook.com>
2023-02-12 18:48:46 +0300

    feat: add i18n and color-mode usage example

--- app.vue
+++ app.vue
@@ -1,5 +1,42 @@
+<script setup>
+const { locale } = useI18n()
+const colorMode = useColorMode()
+console.log(colorMode.preference)
+</script>
+
 <template>
   <div>
+    <form>
+      <select v-model="locale">
+        <option value="en">en</option>
+        <option value="fr">fr</option>
+      </select>
+      <p>{{ $t("welcome") }}</p>
+    </form>
+    <div>
+      <h1>Color mode: {{ $colorMode.value }}</h1>
+      <select v-model="$colorMode.preference">
+        <option value="system">System</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="sepia">Sepia</option>
+      </select>
+    </div>
     <NuxtWelcome />
   </div>
 </template>
+
+<style>
+body {
+  background-color: #fff;
+  color: rgba(0, 0, 0, 0.8);
+}
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
+}
+</style>
