45f90e0847e680bce62b328833d330af37bfb916
Uğur kellecioğlu <ugur.kellecioglu@outlook.com>
2023-02-12 18:48:29 +0300

    chore: add color-mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n", "@nuxtjs/color-mode"],
   i18n: {
     // add `vueI18n` option to `@nuxtjs/i18n` module options
     vueI18n: {
