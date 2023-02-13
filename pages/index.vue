386b32ef92b919b21228541cc3f6ea799f318fdb
Uğur kellecioğlu <ugur.kellecioglu@outlook.com>
2023-02-13 22:06:31 +0300

    feat: add layout
    feat: add index page

---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { useCounterStore } from "@/store/counterStore"
+import { useClipboard, usePermission } from "@vueuse/core"
+
+const { locale } = useI18n()
+const colorMode = useColorMode()
+console.log(colorMode.preference)
+
+const counterStore = useCounterStore()
+
+const input = ref("")
+const { text, isSupported, copy } = useClipboard()
+const permissionRead = usePermission("clipboard-read")
+const permissionWrite = usePermission("clipboard-write")
+</script>
+
+<template>
+  <div>
+    <div>
+      <form>
+        <select v-model="locale">
+          <option value="en">en</option>
+          <option value="fr">fr</option>
+        </select>
+        <p>{{ $t("welcome") }}</p>
+      </form>
+    </div>
+    <div>
+      <h1>Color mode: {{ $colorMode.value }}</h1>
+      <select v-model="$colorMode.preference">
+        <option value="system">System</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="sepia">Sepia</option>
+      </select>
+    </div>
+    <div>
+      <h1>Counter: {{ counterStore.count }}</h1>
+      <button @click="counterStore.increment">Increment</button>
+    </div>
+    <div v-if="isSupported">
+      <note>
+        Clipboard Permission: read <b>{{ permissionRead }}</b> | write
+        <b>{{ permissionWrite }}</b>
+      </note>
+      <p>
+        Current copied: <code>{{ text || "none" }}</code>
+      </p>
+      <input v-model="input" type="text" />
+      <button @click="copy(input)">Copy</button>
+    </div>
+    <p v-else>Your browser does not support Clipboard API</p>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #fff;
+  color: rgba(0, 0, 0, 0.8);
+}
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
+}
+</style>
