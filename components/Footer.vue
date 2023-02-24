114898a9960e6713091754d1b89a1d51097e005d
Uğur kellecioğlu <ugur.kellecioglu@outlook.com>
2023-02-24 16:10:00 +0300

    style: landing page list card
    chore: add nuxt icons to the list
    
    chore: proper urls of packages

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,26 +1,30 @@
 <template>
-  <footer class="text-center bg-gray-900 text-white">
-    <div class="container px-6 pt-6">
+  <footer
+    class="text-center border-t-2 border-gray-100 dark:border-gray-600 text-white dark:bg-gray-900 sepia:bg-gray-200 dark:text-gray-100"
+  >
+    <div class="container px-6 pt-6 m-auto">
       <div class="flex justify-center mb-6">
         <a
           v-for="link in socials"
+          :key="link"
           href="#!"
           type="button"
-          class="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1"
+          class="rounded-full border-2 border-gray-200 dark:border-gray-400 text-gray-500 dark:text-gray-400 leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1 flex items-center justify-center"
         >
-          <NuxtIcon :name="`social/${link}`" class="w-3 h-full mx-auto" />
+          <NuxtIcon
+            :name="`social/${link}`"
+            class="w-3 h-full mx-auto flex items-center justify-center"
+          />
         </a>
         <ColorSwitchBox />
       </div>
     </div>
 
-    <div class="text-center p-4">
-      © {{ new Date().getFullYear() }} Copyright:
-      <a
-        class="text-whitehite"
-        href="https://www.linkedin.com/in/ugurkellecioglu/"
-        >Uğur Kellecioğlu</a
-      >
+    <div class="text-center p-4 text-gray-500 dark:text-gray-400">
+      <p>Made with ❤️</p>
+      <p>
+        Licensed under the <a href="https://opensource.org/licenses/MIT">MIT</a>
+      </p>
     </div>
   </footer>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,31 +2,35 @@
 const basedOnList = [
   {
     title: "Nuxt 3",
-    link: "https://v3.nuxtjs.org/",
-  },
-  {
-    title: "Color Mode",
-    link: "https://vueuse.org/core/colorMode/",
-  },
-  {
-    title: "Vueuse",
-    link: "https://vueuse.org/",
-  },
-  {
-    title: "I18n",
-    link: "https://vueuse.org/core/i18n/",
-  },
-  {
-    title: "Nuxt Auth",
-    link: "https://auth.nuxtjs.org/",
+    link: "https://nuxt.com/",
   },
   {
     title: "Tailwind CSS",
     link: "https://tailwindcss.com/",
   },
   {
+    title: "Nuxt Auth",
+    link: "https://auth.nuxtjs.org/",
+  },
+  {
     title: "Pinia",
     link: "https://pinia.esm.dev/",
+  },
+  {
+    title: "Nuxt Icons",
+    link: "https://github.com/gitFoxCode/nuxt-icons",
+  },
+  {
+    title: "I18n",
+    link: "https://sidebase.io/nuxt-auth/",
+  },
+  {
+    title: "VueUse",
+    link: "https://vueuse.org/",
+  },
+  {
+    title: "Color Mode",
+    link: "https://color-mode.nuxtjs.org/",
   },
 ];
 </script>
@@ -35,17 +39,30 @@ const basedOnList = [
   <div
     class="min-h-screen py-2 dark:bg-gray-900 sepia:bg-gray-200 dark:text-white"
   >
-    <div class="justify-center py-2 mt-40 px-40 mx-auto text-center">
-      <h1 class="text-9xl font-bold dark:text-white">LB Nuxt 3 Template</h1>
-      <p class="mt-20 text-2xl font-bold text-gray-700 dark:text-gray-300">
-        This template is based on:
+    <div
+      class="justify-center py-2 xl:mt-28 lg:mt-20 md:mt-12 sm:mt-8 mt-4 xl:px-30 lg:px-20 md:px-10 sm:px-5 px-2 mx-auto text-center"
+    >
+      <h1
+        class="xl:text-9xl lg:text-7xl md:text-5xl sm:text-3xl text-3xl font-bold dark:text-white"
+      >
+        LB Nuxt 3 Template
+      </h1>
+      <p class="mt-20 text-2xl font-thin text-gray-700 dark:text-gray-300">
+        This template is here thanks to the awesome work of the following
+        projects
       </p>
       <ul
-        class="mt-10 text-2xl font-bold text-gray-700 dark:text-gray-300 grid grid-cols-2"
+        class="mt-10 text-2xl font-bold text-gray-700 dark:text-gray-300 grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 cols-1 xl:px-96 lg:px-16 px-7 xl:gap-5 lg:gap-4 md:gap-3 sm:gap-5 gap-5"
       >
-        <li v-for="item in basedOnList" :key="item.title">
-          <a :href="item.link" target="_blank">{{ item.title }}</a>
-        </li>
+        <a
+          v-for="item in basedOnList"
+          :key="item.title"
+          :href="item.link"
+          target="_blank"
+          class="border-2 p-10 rounded-xl cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700 sepia:hover:bg-gray-300 hover:border-gray-400 dark:hover:border-gray-600 sepia:hover:border-gray-400 hover:shadow-lg dark:hover:shadow-lg sepia:hover:shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 dark:hover:scale-110 sepia:hover:scale-110 dark:hover:translate-y-1 sepia:hover:translate-y-1"
+        >
+          <li>{{ item.title }}</li>
+        </a>
       </ul>
     </div>
   </div>
